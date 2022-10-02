66389f61bc2b29b38f59e286c7cd360054883337
Mikhail Suslikov <msuslikov@gmail.com>
2022-10-02 03:00:42 +0300

    connection change to properties use, add database.properties.origin file

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,19 +1,79 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private static Connection connection;
+    private static Statement statement;
+    private static ResultSet resultSet;
+
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        try {
+            connection = Util.getMySQLConnection();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println("Something wrong with connection to database");
+            throw new RuntimeException(e);
+        }
+        try {
+            statement = connection.createStatement();
+            statement.execute(
+                    "CREATE TABLE users ('id' BIGINT PRIMARY KEY AUTO_INCREMENT, 'name' VARCHAR(64), " +
+                        "'lastname' VARCHAR(64), 'age' TINYINT);");
+
+
+        connection.commit();
+
+        } catch (SQLException e) {
+            Util.rollbackQuietly(connection);
+            throw new RuntimeException(e);
+
+        } finally {
+            Util.closeQuietly(resultSet);
+            Util.closeQuietly(statement);
+            Util.closeQuietly(connection);
+        }
 
     }
 
     public void dropUsersTable() {
+        try {
+            connection = Util.getMySQLConnection();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println("Something wrong with connection to database");
+            throw new RuntimeException(e);
+        }
+        try {
+            statement = connection.createStatement();
+            statement.execute(
+                    "DROP TABLE users;");
+
+
+            connection.commit();
+
+        } catch (SQLException e) {
+            Util.rollbackQuietly(connection);
+            throw new RuntimeException(e);
+
+        } finally {
+            Util.closeQuietly(resultSet);
+            Util.closeQuietly(statement);
+            Util.closeQuietly(connection);
+        }
 
     }
 
@@ -26,7 +86,39 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userlist = new ArrayList<>();
+
+        try {
+            connection = Util.getMySQLConnection();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println("Something wrong with connection to database");
+            throw new RuntimeException(e);
+        }
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT id, name, lastname, age FROM users;");
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String lastname = resultSet.getString("lastname");
+                Byte age = resultSet.getByte("age");
+                User user = new User(name, lastname, age);
+                user.setId(id);
+                userlist.add(user);
+            }
+            connection.commit();
+            return userlist;
+
+        } catch (SQLException e) {
+            Util.rollbackQuietly(connection);
+            throw new RuntimeException(e);
+
+        } finally {
+            Util.closeQuietly(resultSet);
+            Util.closeQuietly(statement);
+            Util.closeQuietly(connection);
+       }
     }
 
     public void cleanUsersTable() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,80 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    // Connect to MySQL
+    public static Connection getMySQLConnection() throws SQLException,
+            ClassNotFoundException {
+        Properties properties = new Properties();
+
+        try (InputStream in = Files.newInputStream(Paths.get("database.properties"))) {
+            properties.load(in);
+        } catch (IOException e) {
+            System.err.println("Can't get login db properties");
+            throw new RuntimeException();
+        }
+
+        String hostName = properties.getProperty("hostName");
+        String dbName = properties.getProperty("dbName");
+        String userName = properties.getProperty("userName");
+        String password = properties.getProperty("password");
+
+        Connection connection = getMySQLConnection(hostName, dbName, userName, password);
+        connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+        connection.setAutoCommit(false);
+        return connection;
+    }
+
+    public static Connection getMySQLConnection(String hostName, String dbName,
+                                                String userName, String password) throws SQLException,
+            ClassNotFoundException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+        return DriverManager.getConnection(connectionURL, userName,
+                password);
+    }
+
+    public static void closeQuietly(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                //NOP
+            }
+        }
+    }
+    public static void closeQuietly(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                //NOP
+            }
+        }
+    }
+    public static void closeQuietly(ResultSet resultSet) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                //NOP
+            }
+        }
+    }
+    public  static  void rollbackQuietly(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                //NOP
+            }
+        }
+    }
 }
